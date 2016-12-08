4651d86099ea71f5fb500dc63e8235fedf3be4a3
Daniel Seita <takeshidanny@gmail.com>
2016-12-08 09:33:22 -0800

    Did action logic for custom scenario, have to implement option learning now

--- Custom-Scenarios/two_room_domain.py
+++ Custom-Scenarios/two_room_domain.py
@@ -10,9 +10,9 @@ I'm trying to get it similar to the OpenAI gym interface, with a 'step' function
 that returns similar stuff. Also, the number of non-terminal actions is
 hard-coded at 9 and follows numpad conventions:
 
-7 8 9
-4 5 6
-1 2 3
+6 7 8
+3 4 5
+0 1 2
 
 So 5 is the NO-OP action, for instance. The special termination action is
 indicated by -1. The grid is represented with a grid such as:
@@ -34,7 +34,8 @@ S = start
 A = agent
 
 Though actually, there probably isn't any reason for me to use the starting
-state if the agent will be there.
+state if the agent will be there. UPDATE: Actually wait, we do want it if we
+need to reset the scenario.
 
 Status: WIP
 """
@@ -44,39 +45,59 @@ import sys
 np.set_printoptions(suppress=True)
 
 A_TERM = -1
-WALL = "X"
-OPEN = "_"
-AGENT = "A"
-GOAL = "G"
+WALL   = "X"
+OPEN   = "_"
+AGENT  = "A"
+GOAL   = "G"
 
 class TwoRooms:
 
-    def __init__(self, length):
+    def __init__(self, length=9):
         """ Initializes the state. See elsewhere for details. """
         assert length >= 3, "assert={} is too low".format(length)
         self.length = length
         self.num_acts = 9
-
-        # Start with the grid
         self.grid = np.zeros((self.length,self.length), dtype=str)
-        self.s_start = (0,0)
-        self.s_agent = (0,0)
-        self.s_goal = (self.length-1,self.length-1)
-        self._init_grid()
+        self.s_start, self.s_agent, self.s_goal = self._init_grid()
 
 
     def _init_grid(self):
         """ Initializes the grid. Currently works best for multiples of 3 which
-        are also odd. """
+        are also odd. For now let's only test on 9x9 grids. """
+
         self.grid.fill(OPEN)
         w1 = np.maximum((self.length/3), 1)
         w2 = np.minimum(2*(self.length/3), self.length)
         self.grid[:, w1:w2].fill(WALL)
-        self.grid[:, w1:w2].fill(WALL)
         self.grid[self.length/2, :].fill(OPEN)
-        assert self.s_agent != self.s_goal
-        self.grid[self.s_agent] = AGENT
-        self.grid[self.s_goal] = GOAL
+
+        sx = np.random.randint(0, self.length)
+        sy = np.random.randint(0, w1)
+        gx = np.random.randint(0, self.length)
+        gy = np.random.randint(w2, self.length)
+        s_agent = (sx,sy)
+        s_goal = (gx,gy)
+
+        assert s_agent != s_goal
+        assert self.grid[s_agent] != WALL
+        assert self.grid[s_goal] != WALL
+        self.grid[s_agent] = AGENT
+        self.grid[s_goal] = GOAL
+        s_start = s_agent
+        return s_start, s_agent, s_goal
+
+
+    def _check_coords_and_move(self, coord):
+        """ Checks if the coordinates are valid. If true, move the agent there.
+        Otherwise, we don't move. """
+        if (coord[0] < 0 or coord[0] >= self.length or \
+            coord[1] < 0 or coord[1] >= self.length or \
+            self.grid[coord] == WALL):
+            pass
+        else:
+            self.grid[self.s_agent] = OPEN
+            self.s_agent = coord
+            self.grid[self.s_agent] = AGENT
 
 
     def step(self, action):
@@ -88,17 +109,33 @@ class TwoRooms:
             action: The action to be taken by the agent.
         """
 
-        # TODO Check logic for action, bumping into wall, etc.
-        
-        observation = self.grid
+        if action == 0:
+            self._check_coords_and_move((self.s_agent[0]+1, self.s_agent[1]-1))
+        elif action == 1:
+            self._check_coords_and_move((self.s_agent[0]+1, self.s_agent[1]))
+        elif action == 2:
+            self._check_coords_and_move((self.s_agent[0]+1, self.s_agent[1]+1))
+        elif action == 3:
+            self._check_coords_and_move((self.s_agent[0], self.s_agent[1]-1))
+        elif action == 4:
+            pass
+        elif action == 5:
+            self._check_coords_and_move((self.s_agent[0], self.s_agent[1]+1))
+        elif action == 6:
+            self._check_coords_and_move((self.s_agent[0]-1, self.s_agent[1]-1))
+        elif action == 7:
+            self._check_coords_and_move((self.s_agent[0]-1, self.s_agent[1]))
+        elif action == 8:
+            self._check_coords_and_move((self.s_agent[0]-1, self.s_agent[1]+1))
+
         cost = 1
-        done = (action == A_TERM)
-        return (observation, cost, done, _)
+        done = (action == A_TERM or self.s_agent == self.s_goal)
+        return (self.grid, cost, done, None)
 
 
     def reset(self):
         """ Resets the environment, like OpenAI gym. """
-        self.s_agent = self.s_start
+        self.s_start, self.s_agent, self.s_goal = self._init_grid()
 
 
     def render(self):
@@ -110,18 +147,33 @@ class TwoRooms:
     def action_space_sample(self):
         """ This is meant to be the equivalent of OpenAI gym's
         action_space.sample, except it's in one method for simplicity.  """
-        return np.random.randint(low=0, high=self.num_actions)
+        return np.random.randint(low=0, high=self.num_acts)
 
 
     def _pretty_print(self):
         print(self.grid)
 
 
+
+def test_nine_rooms():
+    # Test the basic 9 room set-up.
+    env = TwoRooms(9)
+    print("Initial environment")
+    env._pretty_print()
+
+    for i in range(100):
+        a = env.action_space_sample()
+        print("\nTaking {}-th action a={}. Here's the environment:".format(i,a))
+        (_, _, done, _) = env.step(a)
+        env._pretty_print()
+        if done:
+            break
+
+    env.reset()
+    print("\nAfter resetting the environment:")
+    env._pretty_print()
+
+
 if __name__ == "__main__":
     """ Some testing code. """
-    for i in range(3,15+1,3):
-        print("\nEnv w/i={}".format(i))
-        env = TwoRooms(i)
-        env._pretty_print()
-        env.reset()
-        env._pretty_print()
+    test_nine_rooms()
