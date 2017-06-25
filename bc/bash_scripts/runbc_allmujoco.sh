75d2dc407a1bb179808a262147d9ea0855b2f953
DanielTakeshi <takeshidanny@gmail.com>
2017-06-25 10:08:48 -0700

    I had an error in the runbc_allmujoco.sh script.
    I forgot, I didn't have a 25 rollout data for Reacher. So I modified runbc_allmujoco for that, but I don't want to redo what I did so I set up runbc_whoops.sh which contains the rest of the commands that didn't run. So once runbc_allmujoco is done (should be 1-2 days) then I can remove that and the data I have would have effectively been generated from runbc_allmujoco.sh. Sorry.

--- bc/bash_scripts/runbc_allmujoco.sh
+++ bc/bash_scripts/runbc_allmujoco.sh
@@ -2,11 +2,15 @@
 set -eux
 for e in 4 11 18 25; do
     for s in 0 1 2; do
-        python bc.py Reacher-v1     $e --test_rollouts 50 --eval_freq 100 --train_iters 20001 --seed $s --subsamp_freq 1
         python bc.py Ant-v1         $e --test_rollouts 50 --eval_freq 100 --train_iters 20001 --seed $s --subsamp_freq 20
         python bc.py HalfCheetah-v1 $e --test_rollouts 50 --eval_freq 100 --train_iters 20001 --seed $s --subsamp_freq 20
         python bc.py Hopper-v1      $e --test_rollouts 50 --eval_freq 100 --train_iters 20001 --seed $s --subsamp_freq 20
         python bc.py Walker2d-v1    $e --test_rollouts 50 --eval_freq 100 --train_iters 20001 --seed $s --subsamp_freq 20 
+    done
+done
+for e in 4 11 18; do
+    for s in 0 1 2; do
+        python bc.py Reacher-v1     $e --test_rollouts 50 --eval_freq 100 --train_iters 20001 --seed $s --subsamp_freq 1
     done
 done
 for e in 80 160 240; do
